c9b0d898244e3a9aad1c69a48d484f82af6f7e81
mattchristenson <matt@rcc.team>
2024-04-23 09:21:23 -0500

    checkpoint

---
+++ demo-2-5.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var num1;
+var num2;
+var num3;
+var answer1;
+
+num1 = 15;
+num2 = -10;
+
+num3 = 7;
+
+
+var num5 = 128;
+
+///num4 = 42;
+
+answer1 = num2 / 2;
+
+console.log(answer1);
+
+console.log(-answer1);
+
+console.log(answer1);
+
+answer1 = -answer1;
+
+console.log(answer1);
